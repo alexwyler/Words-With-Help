5ced96626871407bdc24ef1bf2dd074eaa7480f3
Alex Langenfeld <alangenfeld@wisc.edu>
2012-02-12 14:43:00 -0800

    Upgrade popup ui

--- WebContent/ChromeExtension/popup.js
+++ WebContent/ChromeExtension/popup.js
@@ -3,11 +3,9 @@ var curOptionIdx;
 var board;
 
 function find() {
-
-chrome.extension.onRequest.addListener(
+  chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
-
-      console.log(request);
+      $("#status").html("Game Board Found, Processing Plays...");
       for (var i = 0; i < 7; i++) {
         $("#rack_" + i).html(request.rack[i]);
       }
@@ -16,60 +14,64 @@ chrome.extension.onRequest.addListener(
       clearBoard();
 
       $.ajax({
-		url: "http://10.0.0.6:8080/WordsWithCheats/",
-		type: "POST",
-		data: JSON.stringify(request),
-		dataType: "json",
-		beforeSend: function(x) {
-			if (x && x.overrideMimeType) {
-				x.overrideMimeType("application/j-son;charset=UTF-8");
-			}
-		},
-		success: function(result) {
-			console.log("resuilts");
-        	console.log(result);
-	      	options = result.options;
-	      	curOptionIdx = 0;
-	      	loadOption(options[curOptionIdx]);
-		}
-	});
-});
-
-  chrome.tabs.executeScript(null,
-      {allFrames: true, file:"iframe.js"});
+	  url: "http://10.0.0.6:8080/WordsWithCheats/",
+	  type: "POST",
+	  data: JSON.stringify(request),
+	  dataType: "json",
+	  beforeSend: function(x) {
+	      if (x && x.overrideMimeType) {
+		  x.overrideMimeType("application/j-son;charset=UTF-8");
+	      }
+	  },
+	  success: function(result) {
+	      $("#status").html("Plays Found!");
+	      console.log("resuilts");
+              console.log(result);
+	      options = result.options;
+	      curOptionIdx = 0;
+	      loadOption(options[curOptionIdx]);
+	  }
+      });
+    });
+    
+    chrome.tabs.executeScript(null,
+			      {allFrames: true, file:"iframe.js"});
 }
+find();
+
 
 function loadOption(move) {
-	$("#points").html(move.score);
+    $("#points").html(move.score);
     for (var i in move.plays) {
-         $("#" + move.plays[i].y + "_" + move.plays[i].x).html(move.plays[i].letter).addClass("red");
+        $("#" + move.plays[i].y + "_" + move.plays[i].x).html(move.plays[i].letter).addClass("red");
     }
 }
 
 function clearBoard() {
-	for (var i = 0; i < 15; i++) {
+    for (var i = 0; i < 15; i++) {
         for (var j = 0; j < 15; j++) {
-          var letter = board[j][i];
-          letter = letter == null ? "_" : letter;
-          tile = $("#" + i + "_" + j);
-          tile.removeClass("red");
-          tile.html(letter);
+            var letter = board[j][i];
+            letter = letter == null ? "_" : letter;
+            tile = $("#" + i + "_" + j);
+            tile.removeClass("red");
+            tile.html(letter);
         }
-      }
+    }
 }
 
 function prevOption() {
-	if (curOptionIdx > 0) {
-		clearBoard();
-		curOptionIdx--;
-		loadOption(options[curOptionIdx]);
-	}
+    if (curOptionIdx > 0) {
+	clearBoard();
+	curOptionIdx--;
+	loadOption(options[curOptionIdx]);
+    }
 }
 
 function nextOption() {
-	if (curOptionIdx < options.length - 1) {
-		clearBoard();
-		curOptionIdx++;
-		loadOption(options[curOptionIdx]);
-	}
+    if (curOptionIdx < options.length - 1) {
+	clearBoard();
+	curOptionIdx++;
+	loadOption(options[curOptionIdx]);
+    }
 }
+
