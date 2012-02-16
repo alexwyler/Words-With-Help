8ebba497399604f0d5128b84ee4c0866a12843b9
Alex Langenfeld <alangenfeld@slotwinerx220.thefacebook.com>
2012-02-15 17:35:06 -0800

    Add blanks letter support.

--- WebContent/ChromeExtension/popup.js
+++ WebContent/ChromeExtension/popup.js
@@ -5,103 +5,107 @@ var board;
 function find() {
   chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
-    	
-    	loadingGif = "<img src='loading.gif'/>";
-    	status = $("#status").html("Finding Moves...<br/>" + loadingGif);
-    	
+      
+      loadingGif = "<img src='loading.gif'/>";
+      status = $("#status").html("Finding Moves...<br/>" + loadingGif);
+      
       for (var i = 0; i < 7; i++) {
         $("#rack_" + i).html(request.rack[i]);
       }
-	
+      
       board = request.board;
       clearBoard();
-
-      $.ajax({
+      
+      $.ajax(
+        {
 	  url: "http://ec2-107-22-41-246.compute-1.amazonaws.com/WWH/",
-      type: "POST",
-	  data: JSON.stringify(request),
-	  dataType: "json",
-	  beforeSend: function(x) {
-	      if (x && x.overrideMimeType) {
-		  x.overrideMimeType("application/j-son;charset=UTF-8");
-	      }
+          type: "POST",
+          data: JSON.stringify(request),
+          dataType: "json",
+          beforeSend: function(x) {
+	    if (x && x.overrideMimeType) {
+	      x.overrideMimeType("application/j-son;charset=UTF-8");
+	    }
 	  },
 	  success: function(result) {
-		  if (result.error) {
-			  $("#status").html(result.error);
-		  } else if (result.options.length < 1) {
-			  $("#status").html("No moves found");
-		  } else {
-		      $("#status").html("Moves Found!");
-		      options = result.options;
-		      for (var i = 0; i < options.length; i++) {
-			  $("#options").append(
-			      "<span onclick=\"selectOption(" + i + ")\" id=\"option" + i + "\">" + (i+1) +
-			      " </span>"
-			  );
-		      }
-		      curOptionIdx = 0;
-		      $("#option" + curOptionIdx).addClass("red");
-		      loadOption(options[curOptionIdx]);
-		  }
+	    if (result.error) {
+	      $("#status").html(result.error);
+	    } else if (result.options.length < 1) {
+	      $("#status").html("No moves found");
+	    } else {
+	      $("#status").html("Moves Found!");
+	      options = result.options;
+	      for (var i = 0; i < options.length; i++) {
+		$("#options").append(
+		  "<span onclick=\"selectOption(" + i + ")\" id=\"option" + i + "\">" + (i+1) +
+		    " </span>"
+		);
+	      }
+	      curOptionIdx = 0;
+	      $("#option" + curOptionIdx).addClass("red");
+	      loadOption(options[curOptionIdx]);
+	    }
 	  },
-      error:function (xhr, ajaxOptions, thrownError) {
-    	  $("#status").html("Unable to connect to server");
-      }
-      });
+          error:function (xhr, ajaxOptions, thrownError) {
+    	    $("#status").html("Unable to connect to server");
+          }
+        });
     });
-    chrome.tabs.executeScript(null,
-			      {allFrames: true, file:"iframe.js"});
+  chrome.tabs.executeScript(null,
+			    {allFrames: true, file:"iframe.js"});
 }
 find();
 
 
 function loadOption(move) {
-	$('#scoreContainer').show();
-    $("#points").html(move.score);
-    for (var i in move.plays) {
-        $("#" + move.plays[i].y + "_" + move.plays[i].x).html(move.plays[i].letter).addClass("red");
+  $('#scoreContainer').show();
+  $("#points").html(move.score);
+  for (var i in move.plays) {
+    if (move.plays[i].blankLetter) {
+      $("#" + move.plays[i].y + "_" + move.plays[i].x).html(move.plays[i].letter).addClass("blue");
     }
+    $("#" + move.plays[i].y + "_" + move.plays[i].x).html(move.plays[i].letter).addClass("red");
+  }
 }
 
 function clearBoard() {
-	$('#scoreContainer').hide();
-    for (var i = 0; i < 15; i++) {
-        for (var j = 0; j < 15; j++) {
-            var letter = board[j][i];
-            letter = letter == null ? "_" : letter;
-            tile = $("#" + i + "_" + j);
-            tile.removeClass("red");
-            tile.html(letter);
-        }
+  $('#scoreContainer').hide();
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
+      var letter = board[j][i];
+      letter = letter == null ? "_" : letter;
+      tile = $("#" + i + "_" + j);
+      tile.removeClass("red");
+      tile.html(letter);
     }
+  }
 }
 
 function selectOption(idx) {
-    clearBoard();
-    $("#option" + curOptionIdx).removeClass("red");
-    curOptionIdx = idx;
-    $("#option" + curOptionIdx).addClass("red");
-    loadOption(options[curOptionIdx]);
+  clearBoard();
+  $("#option" + curOptionIdx).removeClass("red");
+  curOptionIdx = idx;
+  $("#option" + curOptionIdx).addClass("red");
+  loadOption(options[curOptionIdx]);
 }
 
 function prevOption() {
-    if (curOptionIdx > 0) {
-	clearBoard();
-	$("#option" + curOptionIdx).removeClass("red");
-	curOptionIdx--;
-	$("#option" + curOptionIdx).addClass("red");
-	loadOption(options[curOptionIdx]);
-    }
+  if (curOptionIdx > 0) {
+    clearBoard();
+    $("#option" + curOptionIdx).removeClass("red");
+    curOptionIdx--;
+    $("#option" + curOptionIdx).addClass("red");
+    loadOption(options[curOptionIdx]);
+  }
 }
 
 function nextOption() {
-    if (curOptionIdx < options.length - 1) {
-	clearBoard();
-	$("#option" + curOptionIdx).removeClass("red");
-	curOptionIdx++;
-	$("#option" + curOptionIdx).addClass("red");
-	loadOption(options[curOptionIdx]);
-    }
+  if (curOptionIdx < options.length - 1) {
+    clearBoard();
+    $("#option" + curOptionIdx).removeClass("red");
+    curOptionIdx++;
+    $("#option" + curOptionIdx).addClass("red");
+    loadOption(options[curOptionIdx]);
+  }
 }
 
