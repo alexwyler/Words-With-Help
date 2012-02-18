d6551e54014044505602963614e4a7c6c675e525
Alex Langenfeld <alangenfeld@langefeldmbp15.local>
2012-02-18 01:27:17 -0800

    config fix

--- WebContent/ChromeExtension/config.js
+++ WebContent/ChromeExtension/config.js
@@ -1,6 +1,7 @@
 var config = {
-	//'url' : "http://ec2-107-22-41-246.compute-1.amazonaws.com/WWH/",
+	 'url' : "http://ec2-107-22-41-246.compute-1.amazonaws.com/WWH/",
 	// 'url' : "http://172.25.100.80:8080/WordsWithCheats/",
-	'url' : "http://127.0.0.1:8080/WordsWithCheats/",
+//	'url' : "http://127.0.0.1:8080/WordsWithCheats/",
+// 'url' : "http://10.0.0.6:8080/WordsWithCheats/",
 	'api' : 2
 };
